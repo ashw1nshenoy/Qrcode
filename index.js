f3f3f17a031e13d9d205ac14a639f452997d73a3
Ashwin <ashwinshenoy20@gmail.com>
2023-09-09 20:25:27 +0530

    changed css

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+import express from "express";
 import path from 'path';
 import { fileURLToPath } from 'url';
 import qr from "qr-image";
 import fs from "fs";
-import express from "express";
 import bodyParser from "body-parser";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
